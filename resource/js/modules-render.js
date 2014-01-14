e28542828a76faef76b2208c888f5ad77bf5c899
Nick Featch <nick.featch@thatcham.org>
2014-01-14 22:27:35 +0000

    add sample queue event

--- resource/js/modules-render.js
+++ resource/js/modules-render.js
@@ -4,6 +4,10 @@ CORE.create_module("render", function(api) {
   return {
     init: function() {
       this.homeNoProjects();  
+
+      api.listen({
+        'log-spin' : this.logSpin 
+      });
     },
 
     homeNoProjects : function() {
@@ -15,6 +19,10 @@ CORE.create_module("render", function(api) {
         question: 'Do I look like a hovercraft pilot ?'
       };
       api.dataRender('question-panel', hello);
+    },
+
+    logSpin : function(status) {
+      console.log("in log spin");
     },
 
     destroy : function() {
