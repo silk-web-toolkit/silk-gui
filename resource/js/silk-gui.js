fef5f4e0422469e52d1dd40e7a81a946bb91c329
Nick Featch <nick@Nicks-MacBook-Pro.local>
2014-09-24 21:31:35 +0100

    improve - synchronous read project list function

--- resource/js/silk-gui.js
+++ resource/js/silk-gui.js
@@ -12,72 +12,99 @@ if (silkPath == undefined) silkPath = process.env.HOME + "/.silk";
 
 var silkGUIProjectList = silkPath + "/spun-projects.txt";
 
-function spin(project) {
+var data = listProjects();
+if (data) {
+  displayProjects(data);
+  silkReload(data[0].split(",")[0]);
+} else {
+  addLog("Please add a Silk Project.", infoCls);
+}
+
+tinymce.init({
+  selector: "textarea",
+  plugins: ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste"],
+  toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
+
+$(document).on('focusin', function(e) {
+  if ($(event.target).closest(".mce-window").length) {
+    e.stopImmediatePropagation();
+  }
+});
+
+function silkReload(project) {
   var msg = "";
 
-  try { fs.statSync(project);}
-  catch (err) {
+  if (silk) silk.kill("SIGHUP");
+
+  try {
+    fs.statSync(project);
+  } catch (err) {
     addLog("Oh Snap! Directory does not exist.", errorCls);
     return;
   }
 
-  try { silk.kill("SIGHUP"); }
-  catch (err) { }
-
-  addLog("Spinning, please wait ...", infoCls);
-
   silk = spawn('silk', [silkReloadArg], {cwd: project});
+
   silk.stdout.on('data', function(data) {
     msg += data;
+
+    addLog("Spinning, please wait ...", infoCls);
+
     if (msg.indexOf("SUCCESS:") !== -1) {
-      spinOutputlogger(project, msg, true);
+      var logger = addLog("Congratulations, your site was successfully spun!", successCls);
+      // Display in browser link.
+      var openLink = createBtn("View", "Preview Spin Link", "btn btn-success", function() {
+        openBrowserWindow(project)
+      });
+      logger.lastChild.appendChild(openLink);
+
       reloadWindowIfOpen();
-      listAndDisplayProjects(spinOnceLoaded = false);
+      displayProjects(listProjects());
       displayDataCRUD(project);
-      msg = "";  // Clear spin msg for next Silk reload.
+
+      msg = "";
     } else if (msg.indexOf("CAUSE:") !== -1 && msg.indexOf(exitMessage) !== -1) {
-      spinOutputlogger(project, msg, false);
-      msg = "";  // Clear spin msg for next Silk reload.
+      var error = msg.substring(msg.indexOf("CAUSE:") + 14,  msg.lastIndexOf(exitMessage) - 4);
+      addLog("Oh Snap! " + error, errorCls);
+      msg = "";
     }
   });
 }
 
-function listAndDisplayProjects(spinOnceLoaded) {
-  fs.readFile(silkGUIProjectList, 'utf8', function (err, data) {
-    var list = document.getElementById('project-list');
-    removeChildElements(list);
-    try {
-      var items = data.split('\n');
-      if (items.length == 0) {
-        addLog("Please add a Silk Project.", infoCls)
-        return;
-      }
-      for (i = 0; i < items.length-1; i++) {
-        var active = "";
-        if (i == 0)  active = "active";
-        var listItem = createListItem(active);
+function listProjects() {
+  try {
+    return fs.readFileSync(silkGUIProjectList, 'utf8').split('\n');
+  } catch (err) {
+    return null;
+  }
+}
 
-        var csv = items[i].split(",");
-        var name = calculateName(csv[0]);
-        var onClick = function() { spin(this.title);};
-        var link = createLink(name, csv[0], onClick);
-        listItem.appendChild(link);
+function displayProjects(items) {
+  var list = document.getElementById('project-list');
+  removeChildElements(list);
 
-        // Check if directory exists
-        try { fs.statSync(csv[0]);}
-        catch (err) {
-          link.className = "project-not-found";
-          link.disabled = true;
-        }
-        list.appendChild(listItem);
-      }
-      if (spinOnceLoaded) {
-        spin(items[0].split(",")[0]);
-      }
-    } catch (err) {
-      addLog("Please add a Silk Project.", infoCls);
+  for (i = 0; i < items.length-1; i++) {
+    var active = "";
+    if (i == 0)  active = "active";
+    var listItem = createListItem(active);
+
+    var csv = items[i].split(",");
+    var name = calculateName(csv[0]);
+    var onClick = function() { silkReload(this.title);};
+    var link = createLink(name, csv[0], onClick);
+    listItem.appendChild(link);
+
+    try { fs.statSync(csv[0]);}
+    catch (err) {
+      link.className = "project-not-found";
+      link.disabled = true;
     }
-  });
+
+    list.appendChild(listItem);
+  }
+
+  return;
 }
 
 function displayDataCRUD(project) {
@@ -131,19 +158,4 @@ function addLog(msg, className) {
   div.className = className;
   logger.appendChild(div);
   return logger;
-}
-
-function spinOutputlogger(dir, msg, success) {
-  if (success) {
-    var logger = addLog("Congratulations, your site was successfully spun!", successCls);
-    // Display in browser link.
-    var openLink = createBtn("View", "Preview Spin Link", "btn btn-success", function() {
-      openBrowserWindow(dir)
-    });
-    logger.lastChild.appendChild(openLink);
-  } else {
-    // Also removes ASCII color values.
-    var error = msg.substring(msg.indexOf("CAUSE:") + 14,  msg.lastIndexOf(exitMessage) - 4);
-    addLog("Oh Snap! " + error, errorCls);
-  }
 }
