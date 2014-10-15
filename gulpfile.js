cedc79e917de0663fd1012765fd1b52f83d49d65
Nick Featch <Nick.Featch@passivsystems.com>
2014-10-15 13:57:14 +0100

    gulp nw fix for linux

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var NwBuilder = require('node-webkit-builder');
+var gulp = require('gulp');
+
+gulp.task('nw', function () {
+  var nw = new NwBuilder({
+    version: 'latest',
+    files: ['./site/**'],
+//    macIcns: './icons/icon.icns',
+//    platforms: ['win', 'osx', 'linux32', 'linux64']
+    platforms: ['linux64']
+  });
+
+  // Log stuff you want
+  nw.on('log', function (msg) {
+    console.log('node-webkit-builder', msg);
+  });
+
+  // Build returns a promise, return it so the task isn't called in parallel
+  return nw.build().catch(function (err) {
+    console.log('node-webkit-builder', err);
+  });
+});
